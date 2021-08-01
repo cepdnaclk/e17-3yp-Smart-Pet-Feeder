374f9ef0b4335fa592f13f90d037e0a986af19bb
unknown <e17284@eng.pdn.ac.lk>
2021-08-01 23:34:15 +0530

    added 3D design

--- Frontend/src/components/Design/design.js
+++ Frontend/src/components/Design/design.js
@@ -1,13 +1,35 @@
 import React from "react";
-import HeadingSection from "../../components/HeadingSection/HeadingSection";
+import HeadingSection from "../HeadingSection/HeadingSection";
 import ui_design from "../../assets/images/design/ui_design.png";
+import Pet_feeder from "../../assets/images/design/Pet_feeder.jpg";
+import pet_feeder_2 from "../../assets/images/design/pet_feeder_2.jpg";
+import Food_container from "../../assets/images/design/Food_container.jpg";
 
 const Design = () => (
   <section id="design">
     <div className="container">
       <div className="row">
         <HeadingSection title="3D Design Of The Pet Feeder" tagline="">
-          Details of the pet feeder
+          The Pet Feeder
+          <div className="pb-5">
+            <img src={Pet_feeder} />
+          </div>
+          This is the 3D Design of the pet feeder. Mainly it includes a food container and a camera.
+          <div className="pb-5">
+            <img src={pet_feeder_2} />
+          </div>
+
+          <div>
+          The Food Container.
+          </div>
+          <div className="pb-5">
+            <img src={Food_container} />
+          </div>
+          This is a cylindrical shaped food container, which has diveded in to four 
+          partitions and it is rotatable through its axis. 
+          Every partition has a opening at the bottom, and there is a path to the 
+          food plate from the bottom of the cylinder. To serve foods the relevent 
+          partition should be coincided its opening with the path.  
         </HeadingSection>
       </div>
 
