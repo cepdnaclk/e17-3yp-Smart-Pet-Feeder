f74a229a9abf0ad24bc310ed4aff13952fa5d871
unknown <e17284@eng.pdn.ac.lk>
2021-08-28 18:39:31 +0530

    Updated

--- ProjectPage/src/components/Hardware/Hardware.js
+++ ProjectPage/src/components/Hardware/Hardware.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HeadingSection from "../HeadingSection/HeadingSection";
-import Core from "../../assets/images/design/Core.jpg";
+import Core from "../../assets/images/design/Core.JPG";
 import other from "../../assets/images/design/other.jpg";
 import power from "../../assets/images/design/power.jpg";
 import circuit from "../../assets/images/design/Circuit_Diagram.jpg";
@@ -11,24 +11,25 @@ const Hardware = () => (
       <div className="row">
         <div>
           <HeadingSection title="Hardware components" tagline="">
+            
+            <h3>Controller Platform</h3>
             <div className="pb-5">
               <img src={Core} />
             </div>
-            An ESP8266 Node Mcu module, which includes an ESP-12E
-            microcontroller has used as the core hardware component in the Pet
-            feeder. It has many advantages such as; Integrated support for WIFI
-            network, low energy consumption, and low cost, etc.Also, its high
-            processing power with in-built WIFI features make it ideal for IoT
-            projects such as this feeding machine.
+            <p>CPU: 4 x ARM Cortex-A53 , 1.2GHz</p>
+            <p>OS comes pre-loaded with python programming language</p>
+            <p>End nodes  are connected to  hardware interfaces.</p>
+            <p>4GB SD card is used as memory</p>
+            <p>5V main power supply</p>
+
             <div className="pb-5">
               <img src={other} />
             </div>
-            As the other hardware components, Is has included an OV7670 Camera
-            module, 0.91 Inch LCD Display, L298N Dual Bridge DC Motor
+            As the other hardware components, Is has included a Camera Module v1.3 (MD0263),
+            0.91 Inch LCD Display, L298N Dual Bridge DC Motor
             Controllers and 12v Stepper motor. There are some main reasons to
-            use the OV7670 camera module such as its automatic UV adjustment,
-            High sensitivity for low-light, low operating voltage, and ability
-            to get the output as 8-bit RAW RGB data.
+            use the MD0263 camera module such as its high resolution, frame rate and ability 
+            to connect directly to microprocessor through camera serial interface.
             <div className="pb-5">
               <img src={power} />
             </div>
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/e17-3yp-Smart-Pet-Feeder/precache-manifest.50a2bbd278815261463212402093986e.js"
+  "/e17-3yp-Smart-Pet-Feeder/precache-manifest.a4cbf44e34b20485512781f8c3a2b9ac.js"
 );
 
 workbox.clientsClaim();
