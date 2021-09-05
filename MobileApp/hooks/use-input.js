d8e696712e0dada7f0097b68cfabe1c085780d6e
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2021-09-05 15:37:40 +0530

    Mobile App Initial UI is done

---
+++ MobileApp/hooks/use-input.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const useInput = (initialVal, validateValue) => {
+  const [enteredValue, setEnteredValue] = useState(initialVal);
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (value) => {
+    console.log(value);
+    setEnteredValue(value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
