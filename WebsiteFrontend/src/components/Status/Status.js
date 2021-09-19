0c9d3e5523532aa3a723483d1a53a0f5e4581340
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2021-09-19 17:37:44 +0530

    Working on Active scheduling part

--- WebsiteFrontend/src/components/Status/Status.js
+++ WebsiteFrontend/src/components/Status/Status.js
@@ -5,19 +5,13 @@ import VisibilitySensor from "react-visibility-sensor";
 import * as statusActions from "../../store/actions/status";
 
 import Loader from "react-loader-spinner";
-import { API_URL } from "../../configs/Configs";
 import { useDispatch, useSelector } from "react-redux";
-import Page404 from "../../pages/error_page/Page404";
 import Page500 from "../../pages/error_page/Page500";
 
 const Status = ({ bg, type }) => {
   const [viewed, setViewed] = useState(true);
 
-  // const [statusData, setStatusData] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-
   const status = useSelector((state) => state.status.status);
-  const [isRefreshing, setIsRefreshing] = useState(false);
   const dispatch = useDispatch();
 
   const [isLoading, setIsLoading] = useState(false);
@@ -25,81 +19,24 @@ const Status = ({ bg, type }) => {
 
   const loadStatus = useCallback(() => {
     setError(null);
-    // setIsLoading(true);
-    setIsRefreshing(true);
+    setIsLoading(true);
 
     return dispatch(statusActions.fetchStatus())
       .then((response) => {
-        setIsRefreshing(false);
+        setIsLoading(false);
       })
       .catch((err) => {
         setError(err.message);
       });
-  }, [dispatch, setIsRefreshing, setError]);
+  }, [dispatch, setIsLoading, setError]);
 
   useEffect(() => {
-    setIsLoading(true);
-    loadStatus().then(() => {
-      setIsLoading(false);
-    });
+    loadStatus();
   }, [dispatch, loadStatus]);
 
   const viewChangeHandler = (isVisible) => {
     if (isVisible) setViewed(true);
   };
-
-  // const fetchData = useCallback(() => {
-  //   setIsLoading(true);
-  //
-  //
-  //   fetch(API_URL + "/auth/user/get_status", {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: "Bearer " + token,
-  //     },
-  //
-  //   })
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const fetchedData = [
-  //         {
-  //           id: 0,
-  //           value: data.status,
-  //           title: "Status",
-  //           icon: "",
-  //         },
-  //         {
-  //           id: 1,
-  //           value: data.battery,
-  //           title: "Battery",
-  //           icon: "battery-full",
-  //         },
-  //         {
-  //           id: 2,
-  //           value: data.remainingRounds,
-  //           title: "Remaining Rounds",
-  //           icon: "spinner",
-  //         },
-  //         {
-  //           id: 3,
-  //           value: 0,
-  //           title: "Last Feed Before",
-  //           icon: "clock-time",
-  //         },
-  //       ];
-  //       setStatusData(fetchedData);
-  //       setIsLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, [fetchData]);
 
   if (error) {
     return <Page500 path="/user/status" />;
@@ -133,9 +70,6 @@ const Status = ({ bg, type }) => {
                   "count font-700 " + (bg === "white-bg" ? "" : "white-color")
                 }
               >
-                {/*<VisibilitySensor onChange={viewChangeHandler} delayedCall>*/}
-                {/*  <CountUp end={viewed ? counter.value : 0} />*/}
-                {/*</VisibilitySensor>*/}
 
                 {status.status === true ? "ON" : "OFF"}
               </h2>
@@ -222,56 +156,6 @@ const Status = ({ bg, type }) => {
           </div>
         </div>
       )}
-
-      {/*{!isLoading && (*/}
-      {/*  <div className={"container" + (type === "wide" ? "-fluid" : "")}>*/}
-      {/*    <div className="row">*/}
-      {/*      {statusData.map((counter, i) => (*/}
-      {/*        <div*/}
-      {/*          key={i}*/}
-      {/*          className="col-md-3 counter text-center col-sm-6 wow fadeTop"*/}
-      {/*          data-wow-delay="0.1s"*/}
-      {/*          data-aos-delay={`${i}00`}*/}
-      {/*          data-aos={"fade-up"}*/}
-      {/*          data-aos-easing={"ease-in-sine"}*/}
-      {/*        >*/}
-      {/*          {i === 0 && (*/}
-      {/*            <Icofont*/}
-      {/*              icon={counter.value === 1 ? "wifi" : "ban"}*/}
-      {/*              className="light-icon font-30px"*/}
-      {/*            />*/}
-      {/*          )}*/}
-
-      {/*          {i !== 0 && (*/}
-      {/*            <Icofont*/}
-      {/*              icon={counter.icon}*/}
-      {/*              className="light-icon font-30px"*/}
-      {/*            />*/}
-      {/*          )}*/}
-      {/*          <h2*/}
-      {/*            className={*/}
-      {/*              "count font-700 " + (bg === "white-bg" ? "" : "white-color")*/}
-      {/*            }*/}
-      {/*          >*/}
-      {/*            {i !== 0 && (*/}
-      {/*              <VisibilitySensor onChange={viewChangeHandler} delayedCall>*/}
-      {/*                <CountUp end={viewed ? counter.value : 0} />*/}
-      {/*              </VisibilitySensor>*/}
-      {/*            )}*/}
-
-      {/*            {i === 0 && (counter.value === true ? "ON" : "OFF")}*/}
-      {/*            {i === 1 && "%"}*/}
-
-      {/*            {i === 3 && "h"}*/}
-      {/*          </h2>*/}
-      {/*          <h3 className={bg === "white-bg" ? "dark-color" : ""}>*/}
-      {/*            {counter.title}*/}
-      {/*          </h3>*/}
-      {/*        </div>*/}
-      {/*      ))}*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*)}*/}
     </section>
   );
 };
