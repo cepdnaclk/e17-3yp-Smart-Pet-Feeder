34eb8b0a308114525cf573f64afafc74a1fac706
Shenal Perera <e17246@eng.pdn.ac.lk>
2021-10-01 17:14:47 +0530

    Edit email template and Added OTP verification for signup

---
+++ BackEnd/middleware/is-auth-verifySignupUser.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req,res,next) =>{
+    const token = req.get('Authorization').split(' ')[1];
+    let decodedToken;
+    try{
+        decodedToken = jwt.verify(token,'SmartSignupVerification');
+    }
+    catch (err){
+        if (err.name === jwt.TokenExpiredError.name){
+            const error = new Error("OTP verification Failed");
+            error.statusCode = 503;
+            throw error;
+        }
+        else{
+            err.statusCode = 500;
+            throw err;
+        }
+
+    }
+    if (!decodedToken){
+        const error = new Error('You are not authenticated!');
+        error.statusCode = 401;
+        throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
+}
