645babb10d310a15f236071ac0b504545fe311a8
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2021-10-25 19:08:43 +0530

    Add security testing screenshots

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -19,7 +19,6 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
     next();
 });
 
