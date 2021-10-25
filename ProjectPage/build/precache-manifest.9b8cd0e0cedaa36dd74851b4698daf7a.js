73e4513ae4540bbcecb174c8ec155721a03077a2
unknown <e17284@eng.pdn.ac.lk>
2021-10-25 19:37:58 +0530

    ProjectPage updated

---
+++ ProjectPage/build/precache-manifest.9b8cd0e0cedaa36dd74851b4698daf7a.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = [
+  {
+    "revision": "020cce25f071899f4ed0",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/css/main.113f6cc7.chunk.css"
+  },
+  {
+    "revision": "020cce25f071899f4ed0",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/js/main.4ee09460.chunk.js"
+  },
+  {
+    "revision": "e51735140928258f3af2",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/js/runtime~main.1d172121.js"
+  },
+  {
+    "revision": "d7faef49c2f4c7bb9afc",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/css/2.7dba0534.chunk.css"
+  },
+  {
+    "revision": "d7faef49c2f4c7bb9afc",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/js/2.aa238b26.chunk.js"
+  },
+  {
+    "revision": "40279d81a169989546fe3ed5cd4a9b35",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/other.40279d81.jpg"
+  },
+  {
+    "revision": "a417d843ea87c082e359a8ab3612c2b4",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/power.a417d843.jpg"
+  },
+  {
+    "revision": "096cd65fed91d5e62a6c262f325e1052",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ui_design.096cd65f.png"
+  },
+  {
+    "revision": "95588710ac5a34c2955de91f140fc72a",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/AWS.95588710.png"
+  },
+  {
+    "revision": "2793adb0c67ff4b023ed78753f42e1b2",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/architecture.2793adb0.png"
+  },
+  {
+    "revision": "d66f8b7652a2ca0c61cfb6e6abd5db69",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/budget.d66f8b76.JPG"
+  },
+  {
+    "revision": "ccbb6718bed030e4daae0e487f8fba22",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/data_flow.ccbb6718.jpg"
+  },
+  {
+    "revision": "27c41a46a19b1fab777d1a8010907abf",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Circuit_Diagram.27c41a46.jpg"
+  },
+  {
+    "revision": "a020eddbcf6c86e30f813ef4b41862e1",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Core.a020eddb.jpg"
+  },
+  {
+    "revision": "ba1797d5733e940b71fcdb74b408791b",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Food_container.ba1797d5.jpg"
+  },
+  {
+    "revision": "b8f6c22558ffc56078ea182e69ca6a73",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Hardware2.b8f6c225.jpg"
+  },
+  {
+    "revision": "f94f4e57332bf382192e6cb87a4d5700",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Pet_feeder.f94f4e57.jpg"
+  },
+  {
+    "revision": "7d5206f360d83d54f7701294e4ee44bd",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/pet_feeder_2.7d5206f3.jpg"
+  },
+  {
+    "revision": "79dd7ed7ca2aa9abbdb7fe8ac8faef36",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/images.79dd7ed7.jpg"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/et-line.26ec3c7d.eot"
+  },
+  {
+    "revision": "2bea85e74624239934653d10a63c88bc",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/et-line.2bea85e7.svg"
+  },
+  {
+    "revision": "98126e3e1238b0f3b941ad285320ce28",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/et-line.98126e3e.ttf"
+  },
+  {
+    "revision": "b01ff252761958325faab1535c90c87f",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/et-line.b01ff252.woff"
+  },
+  {
+    "revision": "c83dc045ed2e098d5b8708018ec4286c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/startup-bg-left.c83dc045.jpg"
+  },
+  {
+    "revision": "c83dc045ed2e098d5b8708018ec4286c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/travel-bg-right.c83dc045.jpg"
+  },
+  {
+    "revision": "c83dc045ed2e098d5b8708018ec4286c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/about-us.c83dc045.jpg"
+  },
+  {
+    "revision": "c83dc045ed2e098d5b8708018ec4286c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/travel-bg-left.c83dc045.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-6.fe77ad16.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "3ed47b8703d0f7bf441f3d5bd877ed24",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg.3ed47b87.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-4.fe77ad16.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-3.fe77ad16.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-9.fe77ad16.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-7.fe77ad16.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-15.fe77ad16.jpg"
+  },
+  {
+    "revision": "6e682f8dbb59d43f6c6b713894541605",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/map-bg.6e682f8d.png"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-18.fe77ad16.jpg"
+  },
+  {
+    "revision": "3f45b45076d844723de8577574409a0c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-20.3f45b450.jpg"
+  },
+  {
+    "revision": "3ed47b8703d0f7bf441f3d5bd877ed24",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-10.3ed47b87.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-12.fe77ad16.jpg"
+  },
+  {
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-1.9eafde0a.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-4.fe77ad16.jpg"
+  },
+  {
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-2.9eafde0a.jpg"
+  },
+  {
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-3.9eafde0a.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-5.fe77ad16.jpg"
+  },
+  {
+    "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-6.7c6a5bd7.jpg"
+  },
+  {
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-9.9eafde0a.jpg"
+  },
+  {
+    "revision": "9eafde0ac30b657839494322709c5f4c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-10.9eafde0a.jpg"
+  },
+  {
+    "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-7.7c6a5bd7.jpg"
+  },
+  {
+    "revision": "7c6a5bd76f11cf6161da7a76b4e050d5",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/title-hero-8.7c6a5bd7.jpg"
+  },
+  {
+    "revision": "fe77ad1699e236c9748f9420e8d6a756",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/parallax-bg-14.fe77ad16.jpg"
+  },
+  {
+    "revision": "e430101033efff9a294eaafecbac846a",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/404.e4301010.gif"
+  },
+  {
+    "revision": "aa00b7f0cb777b2431d57269c2f40538",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/PetFeeder.aa00b7f0.mp4"
+  },
+  {
+    "revision": "613593c0b1230073e6add2298f43d1f9",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/SoftwareTesting_UnitTest.613593c0.mp4"
+  },
+  {
+    "revision": "667af6a74b14f8edea2dc5750b2cddd1",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/ProgressVideo.667af6a7.mp4"
+  },
+  {
+    "revision": "9bfd6883168711d88d91e8b319b9fb0e",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/home.9bfd6883.jpg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/New Microsoft PowerPoint Presentation.d41d8cd9.pptx"
+  },
+  {
+    "revision": "57ad9a2fb6e0751aff550fffc99ee3a1",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Dr_Isuru.57ad9a2f.jpg"
+  },
+  {
+    "revision": "d18fd4663fe791133997f03bcf0952e6",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Dr_Mahanama.d18fd466.jfif"
+  },
+  {
+    "revision": "571956348a31cf442a023bf4173def45",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/achintha.57195634.jpg"
+  },
+  {
+    "revision": "3ea193517243fd80e296d7501d493d2e",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/sachintha.3ea19351.jpg"
+  },
+  {
+    "revision": "53a58b7ced46f342624e31c6c2aed5af",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/shenal.53a58b7c.jpg"
+  },
+  {
+    "revision": "dd22906b1a735813a2aad2a5412a21de",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/team-03.dd22906b.jpg"
+  },
+  {
+    "revision": "db699a61a18c459407b492211435633b",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/Register.db699a61.jpg"
+  },
+  {
+    "revision": "df2ca179447f80a17876696fdab27c05",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/admin_broadcast.df2ca179.jpg"
+  },
+  {
+    "revision": "01b99259575cf312189dbdd6acb9afad",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/admin_feedback.01b99259.jpg"
+  },
+  {
+    "revision": "eee056351ede532edec5cce0cb81f4a9",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/admin_users.eee05635.jpg"
+  },
+  {
+    "revision": "5cb779c79fb49d3037612c1eec44854c",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/home.5cb779c7.jpg"
+  },
+  {
+    "revision": "30d54e1c6ff97269e963fd0cb13a401b",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/login.30d54e1c.jpg"
+  },
+  {
+    "revision": "797dc6156fcf3a925682d05316eeb9a8",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/user_feedback.797dc615.jpg"
+  },
+  {
+    "revision": "85b67fc3a28689e774fa74ceb93f0810",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/user_history.85b67fc3.jpg"
+  },
+  {
+    "revision": "43481945ffbace2b9baef464e2efd9a7",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/user_notification.43481945.jpg"
+  },
+  {
+    "revision": "d11b0354e4116f230c965f1a36fd01d1",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/user_status.d11b0354.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/e17-3yp-Smart-Pet-Feeder/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "d0d1bcc2e3926626b07f233a0acee803",
+    "url": "/e17-3yp-Smart-Pet-Feeder/index.html"
+  }
+];
